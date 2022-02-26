04c0a5c40ab3a1d956db5b1b99b3ef67eacbd89e
kushan3 <w0777326@myscc.ca>
2022-02-25 20:18:28 -0500

    Add project files.

---
+++ kushanShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kushanShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ kushanShop/Models/KushanShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KushanShop.Models
+{
+    public class KushanShop
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+        
+        public String Size { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
